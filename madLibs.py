600aa9cfbd2154afd566e12b951eda92e971d7ef
ICTisthebest <hab001@stge.org.uk>
2025-08-26 09:23:40 +0000

    started on mad libs

---
+++ madLibs.py
@@ -0,0 +1,7 @@
+name = input("Enter a name")
+age = input("Enter an age:")
+food = input("Enter a food")
+place = input("Enter a place")
+hobby = input("Enter a hobby")
+
+print(name, "is", age, "years old and love eating", food, "And lives at", place, "and loves to", hobby)
