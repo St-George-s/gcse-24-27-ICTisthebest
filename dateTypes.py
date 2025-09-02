14bfa853d3380b40cf643130c9215f37d072ad84
ICTisthebest <hab001@stge.org.uk>
2025-09-02 09:23:58 +0000

    Data Types

---
+++ dateTypes.py
@@ -0,0 +1,45 @@
+# Data Types
+name = "Liyana" # This is a string
+myAge = 13 # This is an integer
+height = 0.00000000000000000000001 # This is a decimal number (float/real)
+hasALittleSister = True # This is a boolean (True/False)
+
+# Casting (Changing from one data type to another)
+age = input ("Enter an age:")
+print(age)
+print(age + "10") # Concatenate two strings together (join them)
+ageAsAnInt = int(age)
+print(ageAsAnInt + 10) # Add two integers together (maths addition)
+print("Your age is " + str(ageAsAnInt))
+
+# Data types - more examples 
+stillAnInteger = -4 
+myNumber = "078782282873" # Always store as a string
+
+# Arithmatic operators 
+add = 10 + 10 
+subtract = 10 - 10 
+multiply = 10 * 10
+division = 10 / 10 
+integerDivision = 11 // 10 # Forces output to be an integer 
+print (add, subtract, multiply, division )
+print(integerDivision)
+modulo = 2000 % 501 # Remainder of the division
+print(modulo)
+exponent = 2 ** 3 # To the power of 
+print(exponent)
+
+# Activity 1 - Take two inputs, multiply two together and output answer
+# Activity 2 - Input user's age, output age time 7
+# Activity 3 - Take Radius as input, output volume of sphere ( v = 4/3 x pi x r^3)
+
+# Activity 1 
+age = input("Enter an age:")
+favouriteNumber = input("Enter a favourite number:")
+
+
+
+
+
+
+
---
+++ songmadLibs.py
@@ -0,0 +1,7 @@
+name = input("Enter a name")
+word = input("Enter an word")
+
+print("You made me a you made me a believer believer"+name)
+print("You break me" +word "and build me up, believer believer")
+print(name+ "Oh, let the bullets fly, oh, let them rain My life, my love, my drive, it came from"+name)
+print("You made me a, you made me a believer, believer")
