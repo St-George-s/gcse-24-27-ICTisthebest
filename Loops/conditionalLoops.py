5e4e0de01093cee4c7d50d42c3fc9b7964f95ab3
ICTisthebest <hab001@stge.org.uk>
2025-09-11 11:07:09 +0000

    cond loops

--- Loops/conditionalLoops.py
+++ Loops/conditionalLoops.py
@@ -14,18 +14,18 @@
 #     userInput = input("Enter new code: ")
 # print("Code accepted!")
 
-# Question 13 - Write a Python program to keep asking for the user's age until it is over 14 and print "Age accepted" once the user enters an age over 14. 
-userAge = 14
-userInput = input("Enter an age: ")
-while int(userInput) <= 14:
-    print("Age not accepted. You are too young.")
-    userInput = input("Enter new age: ")
-print("Age accepted!")
+# # Question 13 - Write a Python program to keep asking for the user's age until it is over 14 and print "Age accepted" once the user enters an age over 14. 
+# userAge = 14
+# userInput = input("Enter an age: ")
+# while int(userInput) <= userAge:
+#     print("Age not accepted. You are too young.")
+#     userInput = input("Enter new age: ")
+# print("Age accepted!")
 
-# Question 14 
-password = input("What is your password? ")
-while len(password) <5:
-    password = input("AGAIN, What is your password? ")
+# # Question 14 
+# password = input("What is your password? ")
+# while len(password) <5:
+#     password = input("AGAIN, What is your password? ")
 
 
 
