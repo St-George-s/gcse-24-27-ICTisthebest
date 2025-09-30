0d94068548ef9f2b29fd5c9d5449f888144821c0
ICTisthebest <hab001@stge.org.uk>
2025-09-30 09:25:16 +0000

    If statements and logic

--- Selection/logic.py
+++ Selection/logic.py
@@ -7,6 +7,7 @@
 # elif weather == "rain" and (timeOfDay == "day" or timeOfDay == "night"):
 #     print("Take an umbrella! ")
 
+# # IF STATEMENTS 
 # # Question 1 
 # age = float(input("Enter your age: "))
 # if age >= 18:
@@ -23,11 +24,88 @@
 # else: 
 #     print("You must not 18+ or 15+ movies! ")
 
-# Question 3 
-mood = input("Enter your mood (happy/sad/angry): ")
-if mood =="happy":
-    print("You are happy! ")
-elif mood == "sad":
-    print("You are sad! ")
+# # Question 3 
+# mood = input("Enter your mood (happy/sad/angry): ")
+# if mood =="happy":
+#     print("You are happy! :) ")
+# elif mood == "sad":
+#     print("You are sad! :( ")
+# elif mood == "angry":
+#     print("You are angry! ")
+
+# # Question 4 
+# genre = input("Enter your favourite genre (fantasy/horror/adventure): ")
+# if genre =="fantasy":
+#     print("Lets watch a fantasy movie! ")
+# elif genre == "horror":
+#     print("Lets watch a horror movie!")
+# elif genre == "adventure":
+#     print("Lets watch an adventure movie! ")
+
+# # Question 5 
+# number1 = float(input("Enter a number: "))
+# number2 = float(input("Enter another number: "))
+# operation = input("Enter an opertaion (add/subtract/multiply/divide): ")
+# if operation == "multiply":
+#     print(number1 * number2 )
+# elif operation == "divide":
+#     print(number1/number2 )
+# elif operation == "add":
+#     print(number1 + number2 )
+# elif operation == "subtract":
+#     print(number1 - number2 )
+
+# LOGIC
+# # Question 1
+# weather = input("Enter weather (sunny/rainy): ")
+# temperature = float(input("Enter a tempertaure: "))
+# if weather == "sunny" and temperature > 15:
+#     print("It's sunny and the tempertaure is over 15! Go outside! ")
+
+# else:
+#     print("Stay inside! ")
+
+# # Question 2 
+# weather = input("Enter weather (sunny/rainy): ")
+# temperature = float(input("Enter a tempertaure: "))
+# if weather == "rainy" or temperature > 15:
+#     print("Do not go outside! ")
+
+# else:
+#     print("Go touch grass! ")
+
+# # Question 3 
+# apples = float(input("Enter number of apples: "))
+# if apples <10:
+#     print("Terrible Harvest! ")
+
+# else:
+#     print ("Great Harvest! ")
+
+# Question 4 
+age = float(input("Enter an age: "))
+driversLicense = input("Do you have a driver license (yes/no): ")
+if age <18 and driversLicense == "yes":
+    print("You may go drive! ")
+elif age >18 and driversLicense == "yes":
+    print ("You can't drive! ")
+else:
+    print("Go get a license! ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
