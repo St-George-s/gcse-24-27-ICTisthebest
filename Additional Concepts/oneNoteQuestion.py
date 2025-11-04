c851890040767861b11fb7e9adad58a103775336
ICTisthebest <hab001@stge.org.uk>
2025-11-04 10:05:59 +0000

    HELLO

---
+++ Additional Concepts/oneNoteQuestion.py
@@ -0,0 +1,26 @@
+# # Question 1 
+# userInput = (input("Enter a character: "))
+# print(userInput[0])
+# print(userInput[-1])
+ 
+# # Question 2 
+# myString = (input("Enter a character: "))
+# reverseString = ""
+# for character in myString:
+#     reverseString = character + reverseString
+# print(reverseString)
+
+# Question 6 
+# import random 
+
+# myRandomNumber = random.randint(1, 6)
+# print(myRandomNumber)
+
+# Question 7 
+import random 
+
+myRandomNumber1 = random.randint(1, 6)
+myRandomNumber2 = random.randint(1, 6)
+
+print(str(myRandomNumber1) + "+" + str(myRandomNumber2) + "=")
+print(str(myRandomNumber1 + myRandomNumber2))
---
+++ Additional Concepts/stringManapulation.py
@@ -0,0 +1,9 @@
+myString = "Christams is coming!"
+print(myString[0])
+print(myString[3])
+
+print(myString[0:3])
+print(myString[0:6])
+
+print(myString[-1])
+print(myString[-3:])
