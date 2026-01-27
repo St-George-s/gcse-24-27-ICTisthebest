770a74d2e18c84be832922ff210f9aff398d8819
ICTisthebest <hab001@stge.org.uk>
2026-01-27 10:23:44 +0000

    hey!

--- Coding Project/projecct.py
+++ Coding Project/projecct.py
@@ -1,16 +1,48 @@
-usernameLength = 0
-specialCount = 0
-specialLetters = ["!", "£", "$", "%", "^", "&", "*", "(", ")", "@", "~", "#", ":", "<", ">", "?", "'", ";", ",", ".", "¬", "`"]
+# Username Validation
+
 username = input("Enter your username: ")
+
 
 has_space = False 
 for i in range(len(username)):
     if username[i] == " ":
         has_space = True
 
-while has_space == True: 
-     print ("Please delete spaces in your username! ")
-     username = input("Please enter username again: ")
+has_special = False 
+for i in range(len(username)):
+    if username[i] == " ":
+        has_special = True
+
+for i in range(3):
+    if len(username) <5: 
+        print ("Username is too short! ")
+        username = input("Please enter username again: ")
+    
+    elif has_space == True:
+        print ("Username has spaces! ")
+        username = input("Please enter username again: ")
+
+    elif has_special == True:
+        print ("Username has special characters! ")
+        username = input("Please enter username again: ")
+    
+
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
